27ae81caeaef345637c162b0642a48a9d260becc
iitzbluwu <ypapalexandrou@gmail.com>
2023-05-03 17:34:22 +0200

    movement

---
+++ Unity/Assets/Scenes/Movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    private float moveDirection;
+
+    private void Awake(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+    }
+}

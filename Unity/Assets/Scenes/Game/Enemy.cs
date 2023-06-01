28397ccfffb809e0c74c7270e371e6a9695d4030
iitzbluwu <ypapalexandrou@gmail.com>
2023-06-01 17:16:19 +0200

    fixed Enemy AI

--- Unity/Assets/Scenes/Game/Enemy.cs
+++ Unity/Assets/Scenes/Game/Enemy.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class Enemy : MonoBehaviour
 {
     public int maxHealth = 30;
     int currentHealth;
+
+    public AIPath aiPath;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +18,15 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         //animator.SetFloat("Speed", Mathf.Abs(moveDirection));
+
+        if(aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        } 
+        else if (aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
     }
     public void TakeDamage(int damage)
     {

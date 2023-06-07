2c444a607fe6aa22bc2d4608894de16c56ead27d
iitzbluwu <ypapalexandrou@gmail.com>
2023-06-07 23:11:42 +0200

    test neue AI

---
+++ Unity/Assets/EnemyAI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public GameObject player;
+    public float speed = 5f;
+    public float approachDistance = 2f;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            Vector2 direction = player.transform.position - transform.position;
+
+            if (direction.magnitude > approachDistance)
+            {
+                direction.Normalize();
+                rb.velocity = direction * speed;
+            }
+            else
+            {
+                rb.velocity = Vector2.zero;
+            }
+        }
+    }
+}

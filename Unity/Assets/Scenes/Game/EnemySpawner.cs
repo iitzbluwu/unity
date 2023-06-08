8fc2a1790ed8d839c697fedced4206d5073ed790
iitzbluwu <ypapalexandrou@gmail.com>
2023-06-08 23:57:05 +0200

    Spawn limit selbe seite

--- Unity/Assets/Scenes/Game/EnemySpawner.cs
+++ Unity/Assets/Scenes/Game/EnemySpawner.cs
@@ -1,23 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
     public float spawnInterval = 5f;
-    public float spawnY = 0f;
+    public float spawnY = 0f; // Y-Position des Spawn-Punkts
 
-    void Start()
+    private int consecutiveSpawns = 0; // Anzahl der aufeinanderfolgenden Spawns auf derselben Seite
+    private bool spawnOnLeft = true; // Starte mit Spawn auf der linken Seite
+
+    private void Start()
     {
         InvokeRepeating("SpawnEnemy", 2f, spawnInterval);
     }
 
     private void SpawnEnemy()
     {
-        float spawnX = Random.Range(0, 2) == 0 ? -15f : 15f; // Zufällige Auswahl zwischen -15f und 15f für X-Position
+        float spawnX = spawnOnLeft ? -15f : 15f;
         Vector3 spawnPosition = new Vector3(spawnX, spawnY, 0f);
 
         Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+
+        consecutiveSpawns++;
+        
+        // Überprüfe, ob die maximale Anzahl auf derselben Seite erreicht wurde
+        if (consecutiveSpawns >= 2)
+        {
+            spawnOnLeft = !spawnOnLeft; // Wechsle die Seite
+            consecutiveSpawns = 0; // Setze den Counter zurück
+        }
     }
 }

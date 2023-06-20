2994ac5fbdd6c392e51531b18c89853d2c7d3450
iitzbluwu <ypapalexandrou@gmail.com>
2023-06-20 23:25:31 +0200

    ~

--- Unity/Assets/Scenes/Game/EnemySpawner.cs
+++ Unity/Assets/Scenes/Game/EnemySpawner.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public GameObject enemyPrefab1; // Erstes Enemy Prefab
-    public GameObject enemyPrefab2; // Zweites Enemy Prefab
+    public GameObject enemyPrefab; // Enemy Prefab
+    public GameObject specialEnemyPrefab; // Special Enemy Prefab
     public float spawnInterval = 5f;
-    public float spawnY = 0f; // Y-Position des Spawn-Punkts
+    public float spawnY = 0f; // Y-Position of the spawn point
 
-    private int consecutiveSpawns = 0; // Anzahl der aufeinanderfolgenden Spawns auf derselben Seite
-    private bool spawnOnLeft = true; // Starte mit Spawn auf der linken Seite
+    private int consecutiveSpawns = 0; // Number of consecutive spawns on the same side
+    private bool spawnOnLeft = true; // Start with spawn on the left side
 
     public int counter;
 
@@ -22,19 +22,19 @@ public class EnemySpawner : MonoBehaviour
         float spawnX = spawnOnLeft ? -15f : 15f;
         Vector3 spawnPosition = new Vector3(spawnX, spawnY, 0f);
 
-        GameObject enemyPrefab;
+        GameObject selectedEnemyPrefab;
 
         if (counter < 5)
         {
-            enemyPrefab = enemyPrefab1;
+            selectedEnemyPrefab = enemyPrefab;
         }
         else
         {
-            enemyPrefab = enemyPrefab2;
+            selectedEnemyPrefab = specialEnemyPrefab;
         }
 
-        // Instantiate the enemy prefab with the appropriate spawn position and rotation
-        GameObject enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+        // Instantiate the selected enemy prefab with the appropriate spawn position and rotation
+        GameObject enemy = Instantiate(selectedEnemyPrefab, spawnPosition, Quaternion.identity);
 
         // Flip the enemy's direction if it spawns on the left side
         if (spawnOnLeft)

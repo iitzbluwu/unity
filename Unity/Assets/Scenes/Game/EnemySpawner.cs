d9870ccb152fab01aeb78b78474b8571b9b1965f
iitzbluwu <ypapalexandrou@gmail.com>
2023-06-20 23:13:42 +0200

    test

--- Unity/Assets/Scenes/Game/EnemySpawner.cs
+++ Unity/Assets/Scenes/Game/EnemySpawner.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public GameObject enemyPrefab;
+    public GameObject enemyPrefab1; // Erstes Enemy Prefab
+    public GameObject enemyPrefab2; // Zweites Enemy Prefab
     public float spawnInterval = 5f;
     public float spawnY = 0f; // Y-Position des Spawn-Punkts
 
@@ -16,32 +17,43 @@ public class EnemySpawner : MonoBehaviour
         InvokeRepeating("SpawnEnemy", 2f, spawnInterval);
     }
 
-private void SpawnEnemy()
-{
-    float spawnX = spawnOnLeft ? -15f : 15f;
-    Vector3 spawnPosition = new Vector3(spawnX, spawnY, 0f);
-
-    // Instantiate the enemy prefab with the appropriate spawn position and rotation
-    GameObject enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
-
-    // Flip the enemy's direction if it spawns on the left side
-    if (spawnOnLeft)
+    private void SpawnEnemy()
     {
-        // Get the sprite renderer component of the enemy
-        SpriteRenderer enemyRenderer = enemy.GetComponent<SpriteRenderer>();
+        float spawnX = spawnOnLeft ? -15f : 15f;
+        Vector3 spawnPosition = new Vector3(spawnX, spawnY, 0f);
 
-        // Flip the enemy's sprite horizontally
-        enemyRenderer.flipX = true;
+        GameObject enemyPrefab;
+
+        if (counter < 5)
+        {
+            enemyPrefab = enemyPrefab1;
+        }
+        else
+        {
+            enemyPrefab = enemyPrefab2;
+        }
+
+        // Instantiate the enemy prefab with the appropriate spawn position and rotation
+        GameObject enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+
+        // Flip the enemy's direction if it spawns on the left side
+        if (spawnOnLeft)
+        {
+            // Get the sprite renderer component of the enemy
+            SpriteRenderer enemyRenderer = enemy.GetComponent<SpriteRenderer>();
+
+            // Flip the enemy's sprite horizontally
+            enemyRenderer.flipX = true;
+        }
+
+        consecutiveSpawns++;
+        counter++;
+
+        // Check if the maximum number of consecutive spawns on the same side has been reached
+        if (consecutiveSpawns >= 2)
+        {
+            spawnOnLeft = !spawnOnLeft; // Switch sides
+            consecutiveSpawns = 0; // Reset the counter
+        }
     }
-
-    consecutiveSpawns++;
-
-    // Check if the maximum number of consecutive spawns on the same side has been reached
-    if (consecutiveSpawns >= 2)
-    {
-        spawnOnLeft = !spawnOnLeft; // Switch sides
-        consecutiveSpawns = 0; // Reset the counter
-    }
-}
-
 }

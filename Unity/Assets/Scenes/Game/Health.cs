1f2fe1e949e6781fc4e63580b904880a87f89dee
iitzbluwu <ypapalexandrou@gmail.com>
2023-06-25 19:16:59 +0200

    Healtbar switch

--- Unity/Assets/Scenes/Game/Health.cs
+++ Unity/Assets/Scenes/Game/Health.cs
@@ -11,17 +11,19 @@ public class Health : MonoBehaviour
     public SpriteRenderer health4; 
     public SpriteRenderer health5; 
 
+    public SpriteRenderer health0Alt; 
+    public SpriteRenderer health1Alt; 
+    public SpriteRenderer health2Alt; 
+    public SpriteRenderer health3Alt; 
+    public SpriteRenderer health4Alt; 
+    public SpriteRenderer health5Alt; 
+
     private bool canUpdateHealth;
 
     void Start()
     {
         canUpdateHealth = false;
-        health0.enabled = false;
-        health1.enabled = false;
-        health2.enabled = false;
-        health3.enabled = false;
-        health4.enabled = false;
-        health5.enabled = false;
+        DisableAllHealthSprites();
 
         // Invoke("EnableHealthUpdate", 2f);
         EnableHealthUpdate(); // Uncomment this line to enable immediate health update
@@ -36,66 +38,149 @@ public class Health : MonoBehaviour
     }
 
     void UpdateHealthSprites()
+{
+    int chosenAvatar = PlayerPrefs.GetInt("Avatar");
+
+    if (Player.currentHealth == 5)
     {
-        if (Player.currentHealth == 5)
+        if (chosenAvatar == 2)
         {
-            health0.enabled = true;
-            health1.enabled = true;
-            health2.enabled = true;
-            health3.enabled = true;
-            health4.enabled = true;
-            health5.enabled = true;
-        }
-        else if (Player.currentHealth == 1)
-        {
-            health0.enabled = true;
-            health1.enabled = true;
-            health2.enabled = false;
-            health3.enabled = false;
-            health4.enabled = false;
-            health5.enabled = false;
-        }
-        else if (Player.currentHealth == 2)
-        {
-            health0.enabled = true;
-            health1.enabled = true;
-            health2.enabled = true;
-            health3.enabled = false;
-            health4.enabled = false;
-            health5.enabled = false;
-        }
-        else if (Player.currentHealth == 3)
-        {
-            health0.enabled = true;
-            health1.enabled = true;
-            health2.enabled = true;
-            health3.enabled = true;
-            health4.enabled = false;
-            health5.enabled = false;
-        }
-        else if (Player.currentHealth == 4)
-        {
-            health0.enabled = true;
-            health1.enabled = true;
-            health2.enabled = true;
-            health3.enabled = true;
-            health4.enabled = true;
-            health5.enabled = false;
+            DisableAllHealthSprites();
+            health5Alt.enabled = true;
         }
         else
         {
-            health0.enabled = true;
-            health1.enabled = false;
-            health2.enabled = false;
-            health3.enabled = false;
-            health4.enabled = false;
-            health5.enabled = false;
+            EnableAllHealthSprites(chosenAvatar);
         }
+    }
+    else if (Player.currentHealth == 1)
+    {
+        health0.enabled = true;
+        health1.enabled = true;
+        health2.enabled = false;
+        health3.enabled = false;
+        health4.enabled = false;
+        health5.enabled = false;
+
+        if (chosenAvatar == 2)
+        {
+            DisableAllHealthSprites();
+            health0Alt.enabled = true;
+            health1Alt.enabled = true;
+        }
+    }
+    else if (Player.currentHealth == 2)
+    {
+        health0.enabled = true;
+        health1.enabled = true;
+        health2.enabled = true;
+        health3.enabled = false;
+        health4.enabled = false;
+        health5.enabled = false;
+
+        if (chosenAvatar == 2)
+        {
+            DisableAllHealthSprites();
+            health0Alt.enabled = true;
+            health1Alt.enabled = true;
+            health2Alt.enabled = true;
+        }
+    }
+    else if (Player.currentHealth == 3)
+    {
+        health0.enabled = true;
+        health1.enabled = true;
+        health2.enabled = true;
+        health3.enabled = true;
+        health4.enabled = false;
+        health5.enabled = false;
+
+        if (chosenAvatar == 2)
+        {
+            DisableAllHealthSprites();
+            health0Alt.enabled = true;
+            health1Alt.enabled = true;
+            health2Alt.enabled = true;
+            health3Alt.enabled = true;
+        }
+    }
+    else if (Player.currentHealth == 4)
+    {
+        health0.enabled = true;
+        health1.enabled = true;
+        health2.enabled = true;
+        health3.enabled = true;
+        health4.enabled = true;
+        health5.enabled = false;
+
+        if (chosenAvatar == 2)
+        {
+            DisableAllHealthSprites();
+            health0Alt.enabled = true;
+            health1Alt.enabled = true;
+            health2Alt.enabled = true;
+            health3Alt.enabled = true;
+            health4Alt.enabled = true;
+        }
+    }
+    else
+    {
+        health0.enabled = true;
+        health1.enabled = false;
+        health2.enabled = false;
+        health3.enabled = false;
+        health4.enabled = false;
+        health5.enabled = false;
+
+        if (chosenAvatar == 2)
+        {
+            DisableAllHealthSprites();
+            health0Alt.enabled = true;
+        }
+    }
+}
+
+
+    void EnableAllHealthSprites(int chosenAvatar)
+    {
+        health0.enabled = true;
+        health1.enabled = true;
+        health2.enabled = true;
+        health3.enabled = true;
+        health4.enabled = true;
+        health5.enabled = true;
+
+        if (chosenAvatar == 2)
+        {
+            health0Alt.enabled = true;
+            health1Alt.enabled = true;
+            health2Alt.enabled = true;
+            health3Alt.enabled = true;
+            health4Alt.enabled = true;
+            health5Alt.enabled = true;
+        }
+    }
+
+    void DisableAllHealthSprites()
+    {
+        health0.enabled = false;
+        health1.enabled = false;
+        health2.enabled = false;
+        health3.enabled = false;
+        health4.enabled = false;
+        health5.enabled = false;
+
+        health0Alt.enabled = false;
+        health1Alt.enabled = false;
+        health2Alt.enabled = false;
+        health3Alt.enabled = false;
+        health4Alt.enabled = false;
+        health5Alt.enabled = false;
     }
 
     void EnableHealthUpdate()
     {
         canUpdateHealth = true;
-        health0.enabled = true;
+        EnableAllHealthSprites(PlayerPrefs.GetInt("Avatar"));
     }
 }

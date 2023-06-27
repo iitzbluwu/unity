e00d120319500ee8045b721ae3dba0d73b239eb2
iitzbluwu <ypapalexandrou@gmail.com>
2023-06-27 18:34:04 +0200

    Pausen + Win Screen verschönert

--- Unity/Assets/Scenes/Game/PauseMenu.cs
+++ Unity/Assets/Scenes/Game/PauseMenu.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenuUI;
+    public Text pauseText; // Add a reference to the UI Text object for displaying the pause text
     private bool isPaused = false;
+
+    private void Start()
+    {
+        pauseText.gameObject.SetActive(false); // Initially hide the pause text
+    }
 
     private void Update()
     {
@@ -14,7 +21,7 @@ public class PauseMenu : MonoBehaviour
             else
                 Pause();
         }
-        
+
         pauseMenuUI.SetActive(isPaused);
     }
 
@@ -23,12 +30,15 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
+        pauseText.gameObject.SetActive(false); // Hide the pause text when resuming
     }
 
     public void Pause()
     {
         Time.timeScale = 0f;
         isPaused = true;
+        pauseText.gameObject.SetActive(true); // Show the pause text when pausing
+        pauseText.text = "Pause"; // Set the text to "Pause"
     }
 
     public void ExitToMainMenu()

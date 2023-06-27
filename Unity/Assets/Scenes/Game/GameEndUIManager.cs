17a3b41ba1e7f69a9d9bed1987f865d68776e5ab
iitzbluwu <ypapalexandrou@gmail.com>
2023-06-27 18:29:14 +0200

    Timer in end screen

--- Unity/Assets/Scenes/Game/GameEndUIManager.cs
+++ Unity/Assets/Scenes/Game/GameEndUIManager.cs
@@ -6,19 +6,26 @@ public class GameEndUIManager : MonoBehaviour
 {
     public GameObject gameEndPanel;
     public Text winText;
+    public Text timeText;
     public Button mainMenuButton;
     public Button quitButton;
-    
+
     private int legionaerCount;
+    private float sceneStartTime;
 
     private void Start()
     {
         gameEndPanel.SetActive(false);
+        winText.gameObject.SetActive(false);
+        timeText.gameObject.SetActive(false);
         mainMenuButton.onClick.AddListener(ReturnToMainMenu);
         quitButton.onClick.AddListener(QuitGame);
 
         // Register an event for the legionaer's death
         Enemy.OnLegionaerDeath += HandleLegionaerDeath;
+
+        // Store the scene start time
+        sceneStartTime = Time.time;
     }
 
     private void HandleLegionaerDeath()
@@ -35,24 +42,38 @@ public class GameEndUIManager : MonoBehaviour
     private void EndGame()
     {
         // Delay the activation of the gameEndPanel by 1 second
-        Invoke("ActivateGameEndPanel", 5f);
+        Invoke("ActivateGameEndPanel", 1f);
     }
-    
+
     private void ActivateGameEndPanel()
     {
         gameEndPanel.SetActive(true);
         winText.gameObject.SetActive(true);
+        timeText.gameObject.SetActive(true);
         mainMenuButton.gameObject.SetActive(true);
         quitButton.gameObject.SetActive(true);
         Time.timeScale = 0f; // Freeze the game
+
+        // Calculate the time duration
+        float sceneDuration = Time.time - sceneStartTime;
+
+        // Display the time duration
+        timeText.text = "Time: " + FormatTime(sceneDuration);
 
         // Optional: You can display a victory message in the "winText" UI Text object if desired
         winText.text = "You Win!";
     }
 
+    private string FormatTime(float time)
+    {
+        int minutes = Mathf.FloorToInt(time / 60f);
+        int seconds = Mathf.FloorToInt(time % 60f);
+        return string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
     private void ReturnToMainMenu()
     {
-        SceneManager.LoadScene(0); // Replace "MainMenu" with the name of your main menu scene
+        SceneManager.LoadScene("MainMenu"); // Replace "MainMenu" with the name of your main menu scene
         Time.timeScale = 1f; // Unfreeze the game
     }
 

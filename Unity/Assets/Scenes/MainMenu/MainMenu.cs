fb5bc585fd4e2ea0fc767889ca10871cfd6dd0d6
iitzbluwu <ypapalexandrou@gmail.com>
2023-06-29 21:29:00 +0200

    Main Menu Update

--- Unity/Assets/Scenes/MainMenu/MainMenu.cs
+++ Unity/Assets/Scenes/MainMenu/MainMenu.cs
@@ -1,13 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject triviaWindowPrefab; // Referenz auf das TriviaWindow Prefab
+
     public void Play()
     {
-        Debug.Log("test");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(1);
+    }
+
+    public void Trivia()
+    {
+        GameObject triviaWindow = Instantiate(triviaWindowPrefab); // Erstellt eine Instanz des TriviaWindow Prefabs
+        //triviaWindow.GetComponent<TriviaWindow>().CloseTriviaWindow(); // Ruft die CloseTriviaWindow-Funktion des TriviaWindow-Skripts auf
+    }
+
+    public void Quit()
+    {
+        Application.Quit(); // Beendet das Spiel
     }
 }
---
+++ Unity/Assets/Scenes/MainMenu/TriviaWindow.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class TriviaWindow : MonoBehaviour
+{
+    public void CloseTriviaWindow()
+    {
+        Destroy(gameObject);
+    }
+}

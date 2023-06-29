da832c3adc5605a730be00dd8a57d44e15b0336d
iitzbluwu <ypapalexandrou@gmail.com>
2023-06-29 22:13:54 +0200

    x

--- Unity/Assets/Scenes/MainMenu/TriviaWindow.cs
+++ Unity/Assets/Scenes/MainMenu/TriviaWindow.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TriviaWindow : MonoBehaviour
 {
     public void CloseTriviaWindow()
     {
         Destroy(gameObject);
+    }
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }

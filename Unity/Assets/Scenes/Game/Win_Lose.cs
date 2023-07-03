bec83599f0d9c3a5cb3880d7a9c40e95d1b5609b
iitzbluwu <ypapalexandrou@gmail.com>
2023-07-03 20:52:43 +0200

    Death funkt noch net mit a d

--- Unity/Assets/Scenes/Game/Win_Lose.cs
+++ Unity/Assets/Scenes/Game/Win_Lose.cs
@@ -2,15 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class Win_Lose : MonoBehaviour
+public class DeathScreenManager : MonoBehaviour
 {
-    public void MainMenu()
+    public GameObject deathScreenPanel;
+    public Button mainMenuButton;
+    public Button retryButton;
+
+    private Button selectedButton;
+
+    private void Start()
     {
-        SceneManager.LoadScene(0);
+        deathScreenPanel.SetActive(false);
+        mainMenuButton.onClick.AddListener(ReturnToMainMenu);
+        retryButton.onClick.AddListener(RetryLevel);
+
+        SelectButton(mainMenuButton);
     }
-    public void retry()
+
+    private void Update()
     {
-        SceneManager.LoadScene(3);
+        if (deathScreenPanel.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                if (selectedButton == retryButton)
+                    SelectButton(mainMenuButton);
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                if (selectedButton == mainMenuButton)
+                    SelectButton(retryButton);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+            {
+                ActivateButton();
+            }
+        }
+    }
+
+    public void ShowDeathScreen()
+    {
+        deathScreenPanel.SetActive(true);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void RetryLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void SelectButton(Button button)
+    {
+        if (selectedButton != null)
+        {
+            selectedButton.interactable = true;
+        }
+
+        selectedButton = button;
+        selectedButton.interactable = false;
+    }
+
+    private void ActivateButton()
+    {
+        if (selectedButton != null)
+        {
+            selectedButton.onClick.Invoke();
+        }
     }
 }

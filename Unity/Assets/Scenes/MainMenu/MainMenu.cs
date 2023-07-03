fac529b37646b6e1833f428275d1c26e70f3495b
iitzbluwu <ypapalexandrou@gmail.com>
2023-07-03 19:58:58 +0200

    button in main mit ws

--- Unity/Assets/Scenes/MainMenu/MainMenu.cs
+++ Unity/Assets/Scenes/MainMenu/MainMenu.cs
@@ -1,9 +1,53 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public GameObject triviaWindowPrefab; // Referenz auf das TriviaWindow Prefab
+    public GameObject playButton; // Referenz auf den Play-Button
+    public GameObject triviaButton; // Referenz auf den Trivia-Button
+    public GameObject quitButton; // Referenz auf den Quit-Button
+
+    private GameObject selectedButton; // Der aktuell ausgewählte Button
+
+    private void Start()
+    {
+        SelectButton(playButton); // Am Anfang wird der Play-Button ausgewählt
+    }
+
+    private void Update()
+    {
+        // Navigation zwischen den Buttons mit den Tasten "w" und "s"
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (selectedButton == triviaButton)
+            {
+                SelectButton(playButton);
+            }
+            else if (selectedButton == quitButton)
+            {
+                SelectButton(triviaButton);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (selectedButton == playButton)
+            {
+                SelectButton(triviaButton);
+            }
+            else if (selectedButton == triviaButton)
+            {
+                SelectButton(quitButton);
+            }
+        }
+
+        // Button auswählen, wenn die Leertaste oder die Eingabetaste gedrückt wird
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
+        {
+            ActivateButton();
+        }
+    }
 
     public void Play()
     {
@@ -12,12 +56,33 @@ public class MainMenu : MonoBehaviour
 
     public void Trivia()
     {
-        GameObject triviaWindow = Instantiate(triviaWindowPrefab); // Erstellt eine Instanz des TriviaWindow Prefabs
-        //triviaWindow.GetComponent<TriviaWindow>().CloseTriviaWindow(); // Ruft die CloseTriviaWindow-Funktion des TriviaWindow-Skripts auf
+        GameObject triviaWindow = Instantiate(triviaWindowPrefab);
+        //triviaWindow.GetComponent<TriviaWindow>().CloseTriviaWindow();
     }
 
     public void Quit()
     {
-        Application.Quit(); // Beendet das Spiel
+        Application.Quit();
+    }
+
+    private void SelectButton(GameObject button)
+    {
+        if (selectedButton != null)
+        {
+            // Den vorherigen ausgewählten Button zurücksetzen
+            selectedButton.GetComponent<UnityEngine.UI.Button>().interactable = true;
+        }
+
+        // Den neuen ausgewählten Button markieren
+        selectedButton = button;
+        selectedButton.GetComponent<UnityEngine.UI.Button>().interactable = false;
+    }
+
+    private void ActivateButton()
+    {
+        if (selectedButton != null)
+        {
+            selectedButton.GetComponent<UnityEngine.UI.Button>().onClick.Invoke();
+        }
     }
 }

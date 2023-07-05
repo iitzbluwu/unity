70e393b2148dc9496fabe91f3e2844a398aae5a5
Earthtub3 <niels16062000@gmail.com>
2023-07-05 22:24:38 +0200

    Death fix sort of

--- Unity/Assets/Scenes/Game/Win_Lose.cs
+++ Unity/Assets/Scenes/Game/Win_Lose.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class DeathScreenManager : MonoBehaviour
+public class Win_Lose : MonoBehaviour
 {
     public GameObject deathScreenPanel;
     public Button mainMenuButton;

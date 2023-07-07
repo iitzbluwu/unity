b6c40ebf86e79c49967878eef37e5e7e3dd809f8
Earthtub3 <niels16062000@gmail.com>
2023-07-07 23:35:58 +0200

    Greif_Animation

--- Unity/Assets/Scenes/Game/Greif.cs
+++ Unity/Assets/Scenes/Game/Greif.cs
@@ -9,9 +9,13 @@ public class Greif : MonoBehaviour
 
     private bool canDamage = true;
 
+    public Animator Greif_Ani;
+    public EnemyAI enemyAI;
+
     void Start()
     {
         currentHealth = maxHealth;
+        enemyAI = GetComponent<EnemyAI>();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -35,19 +39,21 @@ public class Greif : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
+        Greif_Ani.SetTrigger("hurt");
         Debug.Log("Schaden am Greif!");
         currentHealth -= damage;
 
         if (currentHealth <= 0)
         {
-            Die();
+            enemyAI.deadge();
+            Greif_Ani.SetBool("dead", true);
+            Invoke("Die",3.0f);
         }
     }
 
     void Die()
     {
         Debug.Log("Greif Ded!");
-
         GetComponent<Collider2D>().enabled = false;
         GetComponent<SpriteRenderer>().enabled = false;
         this.enabled = false; // Deaktiviert das Greif-Skript

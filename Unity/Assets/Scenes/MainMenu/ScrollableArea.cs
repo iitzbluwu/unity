f2b0a201f23fe77f04f2a95709a7f6008bb40096
iitzbluwu <ypapalexandrou@gmail.com>
2023-07-12 22:51:44 +0200

    ~

--- Unity/Assets/Scenes/MainMenu/ScrollableArea.cs
+++ Unity/Assets/Scenes/MainMenu/ScrollableArea.cs
@@ -6,6 +6,11 @@ public class ScrollableArea : MonoBehaviour
 {
     public ScrollRect scrollRect;
     public TextMeshProUGUI textContent;
+    public float scrollSpeed = 100f;
+    public float maxScrollPosition = 0f;
+
+    private bool isScrollingUp;
+    private bool isScrollingDown;
 
     void Start()
     {
@@ -16,13 +21,34 @@ public class ScrollableArea : MonoBehaviour
     {
         RectTransform contentTransform = scrollRect.content;
         contentTransform.sizeDelta = new Vector2(contentTransform.sizeDelta.x, textContent.preferredHeight);
+        maxScrollPosition = contentTransform.sizeDelta.y - scrollRect.viewport.rect.height;
     }
 
     void Update()
     {
-        float scrollValue = Input.GetAxis("Mouse ScrollWheel");
-        Vector2 scrollDelta = new Vector2(0, scrollValue);
+        isScrollingUp = Input.GetKey(KeyCode.UpArrow);
+        isScrollingDown = Input.GetKey(KeyCode.DownArrow);
 
-        scrollRect.content.anchoredPosition += scrollDelta;
+        if (isScrollingUp && scrollRect.content.anchoredPosition.y < maxScrollPosition)
+        {
+            ScrollUp();
+        }
+        else if (isScrollingDown)
+        {
+            ScrollDown();
+        }
+    }
+
+    void ScrollUp()
+    {
+        float scrollAmount = scrollSpeed * Time.deltaTime;
+        scrollRect.content.anchoredPosition += new Vector2(0, scrollAmount);
+        scrollRect.content.anchoredPosition = new Vector2(0, Mathf.Clamp(scrollRect.content.anchoredPosition.y, 0, maxScrollPosition));
+    }
+
+    void ScrollDown()
+    {
+        float scrollAmount = scrollSpeed * Time.deltaTime;
+        scrollRect.content.anchoredPosition -= new Vector2(0, scrollAmount);
     }
 }

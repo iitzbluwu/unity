9b0cda59c9b67104c420b69f2d890e937b2eb09a
iitzbluwu <ypapalexandrou@gmail.com>
2023-07-12 22:25:11 +0200

    ~

--- Unity/Assets/Scenes/MainMenu/ScrollableArea.cs
+++ Unity/Assets/Scenes/MainMenu/ScrollableArea.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ScrollableArea : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public ScrollRect scrollRect;
+    public TextMeshProUGUI textContent;
+
     void Start()
     {
-        
+        AdjustContentSize();
     }
 
-    // Update is called once per frame
+    void AdjustContentSize()
+    {
+        RectTransform contentTransform = scrollRect.content;
+        contentTransform.sizeDelta = new Vector2(contentTransform.sizeDelta.x, textContent.preferredHeight);
+    }
+
     void Update()
     {
-        
+        float scrollValue = Input.GetAxis("Mouse ScrollWheel");
+        Vector2 scrollDelta = new Vector2(0, scrollValue);
+
+        scrollRect.content.anchoredPosition += scrollDelta;
     }
 }
